c09e461dac69c3e34d3a39aab79798367121eae2
maniangel1990 <alukard327@gmail.com>
2018-01-20 15:37:43 -0600

    Rutas

---
+++ src/app/components/producto/producto.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styles: []
+})
+export class ProductoComponent{
+
+  constructor() { }
+
+
+}
