806a6a7f4e556c5b7c0058192ed09e9c1c00b8c7
maniangel1990 <alukard327@gmail.com>
2018-01-22 19:25:11 -0600

    Search

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styles: []
+})
+export class SearchComponent{
+
+  constructor() { }
+
+ 
+
+}
